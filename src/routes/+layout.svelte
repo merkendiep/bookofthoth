fcb43c2aeb0bdeb3c3900e89a4be81351fe282c4
Maurits Erkendiep <maurits@superscanner.nl>
2023-01-21 00:28:56 +0100

    Add basic terminal page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,19 +1,16 @@
 <script>
-	import Header from './Header.svelte';
 	import './styles.css';
 	import '../app.css'
 </script>
 
-<div class="app">
-	<Header />
+<div class="app p-8 bg-base">
+	<div class="artboard artboard-horizontal phone-4 mockup-window border bg-base-300 mx-auto">
+		<div class="p-4 w-full">
+			// Welcome to the book of thoth. use this terminal to find the knowledge you want.
+		</div>
 
-	<main>
-		<slot />
-	</main>
 
-	<footer>
-		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
-	</footer>
+	</div>
 </div>
 
 <style>
@@ -21,34 +18,5 @@
 		display: flex;
 		flex-direction: column;
 		min-height: 100vh;
-	}
-
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 64rem;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
-
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 12px;
-	}
-
-	footer a {
-		font-weight: bold;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 12px 0;
-		}
 	}
 </style>
